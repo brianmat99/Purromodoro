67345e9d4ddef901e5aa4b070ad7308c924300b0
Brian Matamet <bmatamet99@gmail.com>
2026-03-27 23:17:35 -0700

    Rebrand TomatoBar to Purromodoro with cat icons

--- TomatoBar/Timer.swift
+++ TomatoBar/Timer.swift
@@ -98,7 +98,7 @@ class TBTimer: ObservableObject {
             print("url handling error: cannot parse url")
             return
         }
-        guard scheme.caseInsensitiveCompare("tomatobar") == .orderedSame else {
+        guard scheme.caseInsensitiveCompare("purromodoro") == .orderedSame else {
             print("url handling error: unknown scheme \(scheme)")
             return
         }
